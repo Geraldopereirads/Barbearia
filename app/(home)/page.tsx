5ccb240170ea500f727872f7780dadc640876ad7
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-06-15 15:14:24 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -25,17 +25,27 @@ const Home = async () => {
         <Search />
       </div>
 
-      <div>
-        <div className="px-5 mt-6">
-          <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
-            Agendamentos
-          </h2>
-          <BookingItem />
-        </div>
+      <div className="mt-6">
+      <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+      Agendamentos
+        </h2>
+
+        <BookingItem />
       </div>
       <div className="mt-6">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
           Recomendados
+        </h2>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbeshopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">
+          Populares
         </h2>
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
---
+++ app/_components/footer.tsx
@@ -0,0 +1,8 @@
+
+export const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-6 px-5">
+        <p className="text-gray-400 text-xs font-bold opacity-75">2023 Copyright Barbearia</p>
+    </div>
+  )
+}
