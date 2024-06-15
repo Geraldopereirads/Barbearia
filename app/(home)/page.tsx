1eb838d41befe841fce0f925cf0e5ac5327edaec
geraldopereirads <geraldo.pereirads@hotmail.com>
2024-06-14 21:17:29 -0300

    feat: welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export const Home = () => {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá Miguel</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 };
